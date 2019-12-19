e6b1226e53adbbf08fa5f404fa98b6597fe123e3
kowalix007 <kowalix007@gmail.com>
2019-12-19 23:25:03 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
 key = nil
 value = nil
 hash.collect do |name, age|
-   if value == nil || value < age
+   if value == nil || value > age
     value = age 
     key = name
   
